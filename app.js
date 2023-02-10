358ff7ea6e9ef6ea3b5e9ed1c23eea166ce66458
Malgorzata Bozykowska <malgorzata.bozykowska@gmail.com>
2023-02-10 18:32:01 +0100

    add recipe.html

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require("express");
+// const express = require("express");
 
-const app = express();
+// const express = require("express");
 
-app.use(express.static("public"));
+// const app = express();
 
-app.listen(3000, function() {
-    console.log("server started on port 3000");
-});
+const recipeBtn = document.getElementById("recipeBtn");
+console.log(recipeBtn);
+
+// app.use(express.static("public"));
+
+// app.listen(3000, function() {
+//     console.log("server started on port 3000");
+// });
+
+recipeBtn.onclick = function () {
+    console.log("btn was clicked");
+}
