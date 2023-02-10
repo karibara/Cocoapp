e1464d1111977ebd369969c44514cb95059b17a4
Malgorzata Bozykowska <malgorzata.bozykowska@gmail.com>
2023-02-10 20:38:42 +0100

    add express and start server on port 3000

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-// const express = require("express");
 
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
+const app = express();
 
-const recipeBtn = document.getElementById("recipeBtn");
-console.log(recipeBtn);
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
-// app.listen(3000, function() {
-//     console.log("server started on port 3000");
-// });
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+// change doc.getElem. to body parser
+    // const recipeBtn = document.getElementById("recipeBtn");
+    // console.log(recipeBtn);
 
-recipeBtn.onclick = function () {
-    console.log("btn was clicked");
-}
+});
+
+app.listen(3000, function() {
+    console.log("server started on port 3000");
+});
+
+// recipeBtn.onclick = function () {
+//     console.log("btn was clicked");
+// }
